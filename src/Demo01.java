bc9874c7e3ca51d8f6095e0deb84a4b774a54314
jkass <739463857@qq.com>
2019-05-07 16:34:11 +0800

    定义一个静态方法，用于求两个整数的和。

---
+++ src/Demo01.java
@@ -0,0 +1,10 @@
+public class Demo01 {
+    public static int getSum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int result = Demo01.getSum(2, 3);
+        System.out.println(result);
+    }
+}
