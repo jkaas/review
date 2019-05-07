7655e4eeb585369e108ef4a399b66be0a728e5bb
jkass <739463857@qq.com>
2019-05-07 18:45:02 +0800

    接受一个字符串，将字符串中每一个单词的首字母改为大写。

---
+++ src/sfirstb.java
@@ -0,0 +1,16 @@
+public class sfirstb {
+    public static void main(String[] args) {
+        StringBuffer sbn = new StringBuffer("hellow world and happy new year");
+        StringBuffer ss = new StringBuffer("");
+        String s = sbn.toString();
+        String[] sb = s.split(" ");
+        for (int i = 0; i < sb.length; i++) {
+            sb[i] = sb[i].substring(0,1).toUpperCase() + sb[i].substring(1);
+        }
+        for (int i = 0; i < sb.length; i++) {
+            ss.append(sb[i]);
+            ss.append(" ");
+        }
+        System.out.println(ss);
+    }
+}
